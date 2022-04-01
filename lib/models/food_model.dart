c0e059c617f1f0bd5831f60252f059bfa4ab4ace
nuilewis <lewisnuikweh@gmail.com>
2022-04-01 19:22:16 +0100

    created food model

---
+++ lib/models/food_model.dart
@@ -0,0 +1,11 @@
+
+class FoodLog {
+
+String foodName;
+double? calories;
+DateTime?  time;
+List<String>? ingredients;
+
+FoodLog({required this.foodName, this.calories, this.time, this.ingredients});
+
+}
