6163efdc5a391fbb7c93828b7427c569ab269c99
nuilewis <lewisnuikweh@gmail.com>
2022-04-01 19:25:24 +0100

    edited Food Provider and Methods

--- lib/providers/hb_provider.dart
+++ lib/providers/hb_provider.dart
@@ -1,14 +1,15 @@
-import 'package:flutter/cupertino.dart';
-
+import 'package:flutter/material.dart';
 import '../models/hb_model.dart';
 
 class HbData extends ChangeNotifier {
   List<HbLog> totalHbList = [];
+  List<HbLog> timeRangeList = [];
   double? averageHB = 0;
+  double? averageHBOverTimeRange = 0;
   double? sumOfHB = 0;
 
-  void addHB(double HbValue) {
-    totalHbList.add(HbLog(value: HbValue, time: DateTime.now()));
+  void addHB(double hbValue) {
+    totalHbList.add(HbLog(value: hbValue, time: DateTime.now()));
 
     notifyListeners();
   }
@@ -26,16 +27,37 @@ class HbData extends ChangeNotifier {
 
   void calcAllTimeHbAvg() {
     for (HbLog hbLog in totalHbList) {
-      sumOfHB = (sumOfHB! + hbLog.value!);
+      sumOfHB = (sumOfHB! + hbLog.value);
     }
+
+
 
     averageHB = sumOfHB! / totalHbList.length;
 
     notifyListeners();
   }
 
-  void calcAverageOverTimeRange(){
+  ///Calculate the Average Over a Time Range
+  void calcAverageOverTimeRange({required DateTimeRange timeRange}) {
+    double? hbSumOverTimeRange;
+//set time Range List to empty before calculating
+    timeRangeList = [];
 
+    for (HbLog hbLog in totalHbList) {
+      if (hbLog.time.isAfter(timeRange.start) ||
+          hbLog.time.isBefore(timeRange.end)) {
+        //add to timeRange List
+        timeRangeList.add(hbLog);
+      }
+    }
+
+    ///Calculate Average over time range
+    for (HbLog hbLogTimeRange in timeRangeList) {
+      hbSumOverTimeRange = hbSumOverTimeRange! + hbLogTimeRange.value;
+    }
+
+    averageHBOverTimeRange = hbSumOverTimeRange! / timeRangeList.length;
+
+    notifyListeners();
   }
-  
 }
