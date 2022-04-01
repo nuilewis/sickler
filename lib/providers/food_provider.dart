42c1ff440f5b57289e42e4ee23ad5da796f2e209
nuilewis <lewisnuikweh@gmail.com>
2022-04-01 19:23:18 +0100

    created Food Provider and Methods

---
+++ lib/providers/food_provider.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+import 'package:sickler/models/food_model.dart';
+
+class FoodData extends ChangeNotifier{
+
+
+List<FoodLog> foodLoglist = [];
+
+
+void addFoodLog(FoodLog food){
+  foodLoglist.add(food);
+  notifyListeners();
+}
+
+
+
+void removeLastFoodlog(){
+  foodLoglist.removeLast();
+
+  notifyListeners();
+}
+
+void removeFoodlogAtIndex(int index){
+  foodLoglist.removeAt(index);
+
+  notifyListeners();
+}
+///Todo: find out how to calculate calories;
+void calcCalories(){}
+
+
+}
