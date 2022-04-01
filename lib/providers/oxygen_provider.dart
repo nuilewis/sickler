528bd2dfb4242dd9fbabdebe916344227213289f
nuilewis <lewisnuikweh@gmail.com>
2022-04-01 19:23:58 +0100

    created oxygen Provider and Methods

---
+++ lib/providers/oxygen_provider.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../models/oxygen_model.dart';
+
+class OxygenData extends ChangeNotifier {
+  List<OxygenLog> totalOxygenList = [];
+  List<OxygenLog> timeRangeList = [];
+  double? averageOxygen = 0;
+  double? averageOxygenOverTimeRange = 0;
+  double? sumOfOxygen = 0;
+
+  void addOygen(double oxygenValue) {
+    totalOxygenList.add(OxygenLog(value: oxygenValue, time: DateTime.now()));
+
+    notifyListeners();
+  }
+
+  void removeLatestOxygenLog() {
+    totalOxygenList.removeLast();
+    notifyListeners();
+  }
+
+  void removeHBlogAtIndex(int index) {
+    totalOxygenList.removeAt(index);
+
+    notifyListeners();
+  }
+
+  void calcAllTimeHbAvg() {
+    for (OxygenLog hbLog in totalOxygenList) {
+      sumOfOxygen = (sumOfOxygen! + hbLog.value);
+    }
+
+
+
+    averageOxygen = sumOfOxygen! / totalOxygenList.length;
+
+    notifyListeners();
+  }
+
+  ///Calculate the Average Over a Time Range
+  void calcAverageOverTimeRange({required DateTimeRange timeRange}) {
+    double? oxygenSumOverTimeRange;
+//set time Range List to empty before calculating
+    timeRangeList = [];
+
+    for (OxygenLog oxygenLog in totalOxygenList) {
+      if (oxygenLog.time.isAfter(timeRange.start) ||
+          oxygenLog.time.isBefore(timeRange.end)) {
+        //add to timeRange List
+        timeRangeList.add(oxygenLog);
+      }
+    }
+
+    ///Calculate Average over time range
+    for (OxygenLog oxygenLogTimeRange in timeRangeList) {
+      oxygenSumOverTimeRange = oxygenSumOverTimeRange! + oxygenLogTimeRange.value;
+    }
+
+    averageOxygenOverTimeRange = oxygenSumOverTimeRange! / timeRangeList.length;
+
+    notifyListeners();
+  }
+
+  
+}
