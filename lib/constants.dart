118fe70c3a8b49e0a698daaa8172400cf14accfc
nuilewis <lewisnuikweh@gmail.com>
2022-04-11 16:39:35 +0100

    added date time constant maps

--- lib/constants.dart
+++ lib/constants.dart
@@ -84,3 +84,45 @@ AppBarTheme appBarTheme = const AppBarTheme(
         hintStyle: kBody, 
         fillColor: Colors.white.withOpacity(.2),
       );
+
+
+////-----------------DateTime constants ---------------
+///
+ Map<int, String> daysOfWeek ={
+1:"Monday",
+2: "Tuesday",
+3: "Wednesday",
+4:"Thursday",
+5:"Friday",
+6:"Saturday",
+7:"Sunday",
+ };
+
+  Map<int, String> monthsOfYear ={
+1:"January",
+2: "February",
+3: "March",
+4:"April",
+5:"May",
+6:"June",
+7:"July",
+8:"August",
+9:"September",
+10:"October",
+11:"November",
+12: "December",
+ };
+   Map<int, String> monthsOfYearShortened ={
+1:"Jan",
+2: "Feb",
+3: "Mar",
+4:"Apr",
+5:"May",
+6:"Jun",
+7:"Jul",
+8:"Aug",
+9:"Sept",
+10:"Oct",
+11:"Nov",
+12: "Dec",
+ };
